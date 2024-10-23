ef0f6d2343940cbd05fcdfafbb50326a735fda64
houseOftaz <davoz.anastasia@live.com>
2024-10-23 10:34:23 +0200

    first commit to practice scopes

---
+++ scope/index.js
@@ -0,0 +1,16 @@
+var warrior = "Ninjas";
+
+const screamWarrior = () => {
+  let warrior2 = "Samurai";
+  return {
+    shootWarrior: () => {
+      return console.log(warrior, warrior2);
+    },
+  };
+};
+
+const newWarrior = screamWarrior();
+
+newWarrior.shootWarrior();
+
+console.log(warrior, warrior2);
